bc69b6ac8067606b81c4792992d1ed8e5742dd72
Sridhar B <sridharkalaibala@gmail.com>
2016-04-04 04:25:22 +0530

    read me file added.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# twitter
-twitter
+# Twitter API
+
+Introduction
+---
+
+REST api to get hourly tweet counts for given user. 
+So that, we can identify what hour of the day they are most active. 
+
+Installation
+---
+
+- Download as zip and extract into your web directory Ex. /opt/lamp/htdocs/twitter
+- Edit index.php and enter your twitter api credentials. Lines 8 - 11. Ex. $['TWITTER_CONSUMER_KEY'] , secret and etc. 
+  Note:- If your app credentials is not valid, page will through 500 error. 
+- Installation completed. Now you can access your API Endpoints. 
+- You can install through composer also. Just copy only index.php, composer.json and .htaccess files. 
+  composer update and configure app credentials. That's all. 
+
+Demo
+---
+
+- Endpoint 1. http://sridharb.com/twitter/
+- Endpoint 2. http://sridharb.com/twitter/hello/bigcommerce
+- Endpoint 3. http://sridharb.com/twitter/histogram/narendramodi
+
+
+Unit Test
+---
+
+- There is no unitest written for this.
+
+Known issues / limits
+---
+
+- OpenSSL, CURL extensions are required for twitter connection.
+
--- index.php
+++ index.php
@@ -5,10 +5,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app['TWITTER_CONSUMER_KEY'] = '2rPzLUN4s0QEEWocP1H998WeT';
-$app['TWITTER_CONSUMER_SECRET'] = 'M1a2jWFWaOve9G9aEvd6RSzV6VarMS1SN8UB5gd4wxi01t01Jq';
-$app['TWITTER_ACCESS_TOKEN'] = '235091881-PWyjW7mUyIKGB6WAdq4IrCjEKDhbgmrHtk66Y0V4';
-$app['TWITTER_ACCESS_TOKEN_SECRET'] = 'wfC6gcbSA9XnThomHAER8gD1jy1kUVcTD3vGu2WuuMkVR';
+$app['TWITTER_CONSUMER_KEY'] = '';
+$app['TWITTER_CONSUMER_SECRET'] = '';
+$app['TWITTER_ACCESS_TOKEN'] = '';
+$app['TWITTER_ACCESS_TOKEN_SECRET'] = '';
 
 $app->get('/', function() use($app) {
 
@@ -38,7 +38,7 @@ $app->get('/histogram/{username}', function($username) use($app) {
     $request->getQuery()->set('since', date('Y-m-d'));
     $response   = $request->send();
     $tweets     = json_decode($response->getBody(), true);
-    $histogram  = array('day' => date('Y-m-d'));
+    $histogram  = array();
     
     for ($i = 0; $i < 24; $i++) {
         $key             = date('H:00', strtotime(date('Y-m-d') . ' + ' . $i . ' hours')) . '-' . date('H:00', strtotime(date('Y-m-d') . ' + ' . ($i + 1) . ' hours'));
@@ -49,6 +49,10 @@ $app->get('/histogram/{username}', function($username) use($app) {
         $key = date('H:00', strtotime($tweet['created_at'])) . '-' . date('H:00', strtotime($tweet['created_at'] . ' + 1 hours'));
         $histogram[$key] ++;
     }
+    
+    $maxs                     = array_keys($histogram, max($histogram));
+    $histogram['most_active'] = ($maxs[0] != 0 )? implode(',', $maxs) : 'no tweet or invalid user';
+    $histogram['day']         = date('Y-m-d');
 
     return $app->json($histogram);
 });
